de05656d0f97e1a3ffe960d6a61a4751e06fb7a8
Tet <isuru.dev@gmail.com>
2026-05-03 19:00:09 +0530

    syslink test

--- hypr/record-stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-RECORD_DIR="$HOME/Videos"
-TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-OUTPUT="$RECORD_DIR/rec_$TIMESTAMP.mkv"
-
-killall -SIGINT gpu-screen-recorder 2>/dev/null
-sleep 2.5
-notify-send "Screen Recording" "Stopped → $OUTPUT"
